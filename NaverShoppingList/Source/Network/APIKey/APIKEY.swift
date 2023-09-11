276b6b95bfc4d18f2c956fcb7e5a284da8668671
Jimmy <joony300@gmail.com>
2023-09-11 09:30:51 +0900

    [Fix] APIKEY 다시 생성

---
+++ NaverShoppingList/Source/Network/APIKey/APIKEY.swift
@@ -0,0 +1,16 @@
+//
+//  APIKEY.swift
+//  NaverShoppingList
+//
+//  Created by 정준영 on 2023/09/09.
+//
+
+import Foundation
+
+enum APIKEY {
+    static let ClientID: String = "kpG35M5KzdwRH6LIoB08"
+    static let ClientSecret: String = "Aa2YhvSaQm"
+    
+    static let ClientID_Header: String = "X-Naver-Client-Id"
+    static let ClientSecret_Header: String = "X-Naver-Client-Secret"
+}
